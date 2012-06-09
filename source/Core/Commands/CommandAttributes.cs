9537f79c9938678b827b1b1cfad4eabc853901a5
Andreas Koch <andyk7@gmail.com>
2012-06-09 12:03:27 +0200

    Make sure the command attribute properities are always initialized

--- source/Core/Commands/CommandAttributes.cs
+++ source/Core/Commands/CommandAttributes.cs
@@ -4,6 +4,12 @@ namespace NuDeploy.Core.Commands
 {
     public class CommandAttributes
     {
+        public CommandAttributes()
+        {
+            this.CommandName = string.Empty;
+            this.AlternativeCommandNames = new string[] { };
+        }
+
         public string CommandName { get; set; }
 
         public IEnumerable<string> AlternativeCommandNames { get; set; }
