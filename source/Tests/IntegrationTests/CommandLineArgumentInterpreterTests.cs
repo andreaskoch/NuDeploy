846c2c8021d25c930da8de58051de19704f4cd4e
Andreas Koch <andyk7@gmail.com>
2012-06-09 12:24:34 +0200

    Created a first integration test for the CommandLineArgumentInterpreter class

---
+++ source/Tests/IntegrationTests/CommandLineArgumentInterpreterTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+
+using Moq;
+
+using NuDeploy.Core.Commands;
+using NuDeploy.Core.Common;
+
+using NUnit.Framework;
+
+namespace NuDeploy.Tests.IntegrationTests
+{
+    [TestFixture]
+    public class CommandLineArgumentInterpreterTests
+    {
+        private ICommand packackageSolutionCommand;
+
+        private IEnumerable<ICommand> commands;
+
+        private ICommand helpCommand;
+
+        private ICommandLineArgumentInterpreter commandLineArgumentInterpreter;
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            var userInterfaceMock = new Mock<IUserInterface>();
+            this.helpCommand = new HelpCommand(userInterfaceMock.Object);
+
+            this.packackageSolutionCommand = new PackageSolutionCommand();
+            this.commands = new List<ICommand> { this.packackageSolutionCommand, this.helpCommand };
+
+            ICommandProvider commandProvider = new CommandProvider(this.commands);
+            ICommandNameMatcher commandNameMatcher = new CommandNameMatcher();
+            ICommandArgumentParser commandArgumentParser = new CommandArgumentParser();
+            ICommandArgumentNameMatcher commandArgumentNameMatcher = new CommandArgumentNameMatcher();
+
+            this.commandLineArgumentInterpreter = new CommandLineArgumentInterpreter(
+                commandProvider, commandNameMatcher, commandArgumentParser, commandArgumentNameMatcher);            
+        }
+
+        [Test]
+        public void GetCommand_PackageSolutionCommand_FullCommandNameIsSupplied_NoArguments_ResultIsThePackageSolutionCommandWithoutArgumentValues()
+        {
+            // Arrange
+            var arguments = new[] { "package" };
+
+            // Act
+            ICommand result = this.commandLineArgumentInterpreter.GetCommand(arguments);
+
+            // Assert
+            Assert.AreEqual(this.packackageSolutionCommand.Attributes.CommandName, result.Attributes.CommandName);
+            foreach (var key in result.Arguments.Keys)
+            {
+                Assert.IsNull(result.Arguments[key]);
+            }
+        }
+    }
+}
