8c2ac80d4380567c01ff91c1f1d0c388a885ab40
Andreas Koch <andyk7@gmail.com>
2012-06-24 21:36:55 +0200

    Simplified the asyc executor

--- source/Core/PowerShell/PipelineExecutor.cs
+++ source/Core/PowerShell/PipelineExecutor.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Management.Automation;
 using System.Management.Automation.Runspaces;
 using System.Threading;
@@ -13,11 +12,6 @@ namespace NuDeploy.Core.PowerShell
         #region Constants and Fields
 
         /// <summary>
-        ///   The object that is used to invoke the events on.
-        /// </summary>
-        private readonly ISynchronizeInvoke invoker;
-
-        /// <summary>
         ///   The powershell script pipeline that will be executed asynchronously.
         /// </summary>
         private readonly Pipeline pipeline;
@@ -26,26 +20,6 @@ namespace NuDeploy.Core.PowerShell
         ///   Event set when the user wants to stop script execution.
         /// </summary>
         private readonly ManualResetEvent stopEvent;
-
-        /// <summary>
-        ///   Local delegate to a private method
-        /// </summary>
-        private readonly DataEndDelegate synchDataEnd;
-
-        /// <summary>
-        ///   Local delegate to a private method
-        /// </summary>
-        private readonly DataReadyDelegate synchDataReady;
-
-        /// <summary>
-        ///   Local delegate to a private method
-        /// </summary>
-        private readonly ErrorReadyDelegate synchErrorReady;
-
-        /// <summary>
-        ///   Array of waithandles, used in the StoppableInvoke() method.
-        /// </summary>
-        private readonly WaitHandle[] waitHandles;
 
         #endregion
 
@@ -58,31 +32,20 @@ namespace NuDeploy.Core.PowerShell
         /// <param name="runSpace">
         /// Powershell runspace to use for creating and executing the script. 
         /// </param>
-        /// <param name="invoker">
-        /// The object to synchronize the DataReady and DataEnd events with. Normally you'd pass the form or component here. 
-        /// </param>
         /// <param name="command">
         /// The script to run 
         /// </param>
-        public PipelineExecutor(Runspace runSpace, ISynchronizeInvoke invoker, string command)
+        public PipelineExecutor(Runspace runSpace, string command)
         {
-            this.invoker = invoker;
-
-            // initialize delegates
-            this.synchDataReady = this.SynchDataReady;
-            this.synchDataEnd = this.SynchDataEnd;
-            this.synchErrorReady = this.SynchErrorReady;
-
             // initialize event members
             this.stopEvent = new ManualResetEvent(false);
-            this.waitHandles = new WaitHandle[] { null, this.stopEvent };
 
             // create a pipeline and feed it the script text
             this.pipeline = runSpace.CreatePipeline(command);
 
             // we'll listen for script output data by way of the DataReady event
             this.pipeline.Output.DataReady += this.Output_DataReady;
-            this.pipeline.Error.DataReady += new EventHandler(this.Error_DataReady);
+            this.pipeline.Error.DataReady += this.Error_DataReady;
         }
 
         #endregion
@@ -98,11 +61,6 @@ namespace NuDeploy.Core.PowerShell
         #endregion
 
         #region Public Events
-
-        /// <summary>
-        ///   Occurs when powershell script completed its execution.
-        /// </summary>
-        public event DataEndDelegate OnDataEnd;
 
         /// <summary>
         ///   Occurs when there is new data available from the powershell script.
@@ -169,11 +127,9 @@ namespace NuDeploy.Core.PowerShell
         {
             // fetch all available objects
             Collection<object> data = this.pipeline.Error.NonBlockingRead();
-
-            // if there were any, invoke the ErrorReady event
-            if (data.Count > 0)
+            if (this.OnErrorReady != null)
             {
-                this.StoppableInvoke(this.synchErrorReady, new object[] { this, data });
+                this.OnErrorReady(this, data);
             }
         }
 
@@ -188,82 +144,9 @@ namespace NuDeploy.Core.PowerShell
         {
             // fetch all available objects
             Collection<PSObject> data = this.pipeline.Output.NonBlockingRead();
-
-            // if there were any, invoke the DataReady event
-            if (data.Count > 0)
+            if (this.OnDataReady != null)
             {
-                this.StoppableInvoke(this.synchDataReady, new object[] { this, data });
-            }
-
-            if (this.pipeline.Output.EndOfPipeline)
-            {
-                // we're done! invoke the DataEnd event
-                this.StoppableInvoke(this.synchDataEnd, new object[] { this });
-            }
-        }
-
-        /// <summary>
-        /// Special Invoke method that operates similarly to <see cref="ISynchronizeInvoke.Invoke"/> but in addition to that, it can be aborted by setting the stopEvent. This avoids potential deadlocks that are possible when using the regular <see cref="ISynchronizeInvoke.Invoke"/> method.
-        /// </summary>
-        /// <param name="method">
-        /// A <see cref="System.Delegate"/> to a method that takes parameters of the same number and type that are contained in <paramref name="args"/> 
-        /// </param>
-        /// <param name="args">
-        /// An array of type <see cref="System.Object"/> to pass as arguments to the given method. This can be null if no arguments are needed 
-        /// </param>
-        /// <returns>
-        /// The <see cref="Object"/> returned by the asynchronous operation 
-        /// </returns>
-        private object StoppableInvoke(Delegate method, object[] args)
-        {
-            IAsyncResult asyncResult = this.invoker.BeginInvoke(method, args);
-            this.waitHandles[0] = asyncResult.AsyncWaitHandle;
-            return (WaitHandle.WaitAny(this.waitHandles) == 0) ? this.invoker.EndInvoke(asyncResult) : null;
-        }
-
-        /// <summary>
-        /// private DataEnd handling method that will pass the call on to any handlers that are attached to the OnDataEnd event of this <see cref="PipelineExecutor"/> instance.
-        /// </summary>
-        /// <param name="sender">
-        /// </param>
-        private void SynchDataEnd(PipelineExecutor sender)
-        {
-            DataEndDelegate delegateDataEndCopy = this.OnDataEnd;
-            if (delegateDataEndCopy != null)
-            {
-                delegateDataEndCopy(sender);
-            }
-        }
-
-        /// <summary>
-        /// private DataReady handling method that will pass the call on to any event handlers that are attached to the OnDataReady event of this <see cref="PipelineExecutor"/> instance.
-        /// </summary>
-        /// <param name="sender">
-        /// </param>
-        /// <param name="data">
-        /// </param>
-        private void SynchDataReady(PipelineExecutor sender, ICollection<PSObject> data)
-        {
-            DataReadyDelegate delegateDataReadyCopy = this.OnDataReady;
-            if (delegateDataReadyCopy != null)
-            {
-                delegateDataReadyCopy(sender, data);
-            }
-        }
-
-        /// <summary>
-        /// private ErrorReady handling method that will pass the call on to any event handlers that are attached to the OnErrorReady event of this <see cref="PipelineExecutor"/> instance.
-        /// </summary>
-        /// <param name="sender">
-        /// </param>
-        /// <param name="data">
-        /// </param>
-        private void SynchErrorReady(PipelineExecutor sender, ICollection<object> data)
-        {
-            ErrorReadyDelegate delegateErrorReadyCopy = this.OnErrorReady;
-            if (delegateErrorReadyCopy != null)
-            {
-                delegateErrorReadyCopy(sender, data);
+                this.OnDataReady(this, data);
             }
         }
 
--- source/Core/PowerShell/PowerShellScriptExecutor.cs
+++ source/Core/PowerShell/PowerShellScriptExecutor.cs
@@ -1,15 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.IO;
 using System.Management.Automation;
 using System.Management.Automation.Host;
 using System.Management.Automation.Runspaces;
-using System.Threading.Tasks;
 
 namespace NuDeploy.Core.PowerShell
 {
-    public class PowerShellScriptExecutor : IPowerShellScriptExecutor, ISynchronizeInvoke, IDisposable
+    public class PowerShellScriptExecutor : IPowerShellScriptExecutor, IDisposable
     {
         private readonly PSHost powerShellHost;
 
@@ -41,9 +39,8 @@ namespace NuDeploy.Core.PowerShell
                 throw new ArgumentNullException("scriptText");
             }
 
-            this.pipelineExecutor = new PipelineExecutor(this.powerShell.Runspace, this, scriptText);
+            this.pipelineExecutor = new PipelineExecutor(this.powerShell.Runspace, scriptText);
             this.pipelineExecutor.OnDataReady += this.pipelineExecutor_OnDataReady;
-            this.pipelineExecutor.OnDataEnd += this.pipelineExecutor_OnDataEnd;
             this.pipelineExecutor.OnErrorReady += this.pipelineExecutor_OnErrorReady;
             this.pipelineExecutor.Start();
         }
@@ -69,31 +66,6 @@ namespace NuDeploy.Core.PowerShell
             this.ExecuteCommand(commandText);
         }
 
-        private void StopScript()
-        {
-            if (this.pipelineExecutor != null)
-            {
-                this.pipelineExecutor.OnDataReady -= this.pipelineExecutor_OnDataReady;
-                this.pipelineExecutor.OnDataEnd -= this.pipelineExecutor_OnDataEnd;
-                this.pipelineExecutor.Stop();
-                this.pipelineExecutor = null;
-            }
-        }
-
-        private void pipelineExecutor_OnDataEnd(PipelineExecutor sender)
-        {
-            if (sender.Pipeline.PipelineStateInfo.State == PipelineState.Failed)
-            {
-                this.powerShellHost.UI.WriteLine(string.Format("Error in script: {0}", sender.Pipeline.PipelineStateInfo.Reason));
-            }
-            else
-            {
-                this.powerShellHost.UI.WriteLine("Ready.");
-            }
-
-            this.ready();
-        }
-
         private void pipelineExecutor_OnDataReady(PipelineExecutor sender, ICollection<PSObject> data)
         {
             foreach (PSObject obj in data)
@@ -107,29 +79,6 @@ namespace NuDeploy.Core.PowerShell
             foreach (object e in data)
             {
                 this.powerShellHost.UI.WriteLine("Error : " + e.ToString());
-            }
-        }
-
-        public IAsyncResult BeginInvoke(Delegate method, object[] args)
-        {
-            throw new NotImplementedException();
-        }
-
-        public object EndInvoke(IAsyncResult result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public object Invoke(Delegate method, object[] args)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool InvokeRequired
-        {
-            get
-            {
-                throw new NotImplementedException();
             }
         }
 
