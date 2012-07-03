bdc771659705a405446ba692d208b96ee6fa9a77
Andreas Koch <andyk7@gmail.com>
2012-07-03 10:27:46 +0200

    Changed the default feed url to the Nuget Default Url

--- source/Core/Common/NuDeployConstants.cs
+++ source/Core/Common/NuDeployConstants.cs
@@ -6,7 +6,7 @@
 
         public static readonly string ExecutableName = string.Format("{0}.exe", ApplicationName);
 
-        public static readonly string DefaultFeedUrl = "https://nuget.local-application-gallery.org/api/v2/";
+        public static readonly string DefaultFeedUrl = "https://nuget.org/api/v2/";
 
         public static readonly string NuDeployCommandLinePackageId = string.Format("{0}.CommandLine", ApplicationName);
 
