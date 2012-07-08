e78062858268857b7c389153995f1f1a04eefb73
Andreas Koch <andyk7@gmail.com>
2012-07-08 22:38:52 +0200

    Created tests for the ApplicationInformationProvider

---
+++ source/Tests/IntegrationTests/Common/ApplicationInformationProviderTests.cs
@@ -0,0 +1,112 @@
+﻿using System.IO;
+
+using NuDeploy.Core.Common;
+
+using NUnit.Framework;
+
+namespace NuDeploy.Tests.IntegrationTests.Common
+{
+    [TestFixture]
+    public class ApplicationInformationProviderTests
+    {
+        [Test]
+        public void GetApplicationInformation_ApplicationNameIsSet()
+        {
+            // Act
+            var result = ApplicationInformationProvider.GetApplicationInformation();
+
+            // Assert
+            Assert.IsNotNullOrEmpty(result.ApplicationName);
+        }
+
+        [Test]
+        public void GetApplicationInformation_NameOfExecutableIsSet()
+        {
+            // Act
+            var result = ApplicationInformationProvider.GetApplicationInformation();
+
+            // Assert
+            Assert.IsNotNullOrEmpty(result.NameOfExecutable);
+        }
+
+        [Test]
+        public void GetApplicationInformation_ApplicationVersionIsSet()
+        {
+            // Act
+            var result = ApplicationInformationProvider.GetApplicationInformation();
+
+            // Assert
+            Assert.IsNotNull(result.ApplicationVersion);
+        }
+
+        [Test]
+        public void GetApplicationInformation_StartupFolderIsSet()
+        {
+            // Act
+            var result = ApplicationInformationProvider.GetApplicationInformation();
+
+            // Assert
+            Assert.IsNotNullOrEmpty(result.StartupFolder);
+        }
+
+        [Test]
+        public void GetApplicationInformation_StartupFolderIsExistingDirectory()
+        {
+            // Act
+            var result = ApplicationInformationProvider.GetApplicationInformation();
+
+            // Assert
+            Assert.IsTrue(Directory.Exists(result.StartupFolder));
+        }
+
+        [Test]
+        public void GetApplicationInformation_ConfigurationFileFolderIsSet()
+        {
+            // Act
+            var result = ApplicationInformationProvider.GetApplicationInformation();
+
+            // Assert
+            Assert.IsNotNullOrEmpty(result.ConfigurationFileFolder);
+        }
+
+        [Test]
+        public void GetApplicationInformation_ConfigurationFileFolderIsExistingDirectory()
+        {
+            // Act
+            var result = ApplicationInformationProvider.GetApplicationInformation();
+
+            // Assert
+            Assert.IsTrue(Directory.Exists(result.ConfigurationFileFolder));
+        }
+
+        [Test]
+        public void GetApplicationInformation_LogFolderIsSet()
+        {
+            // Act
+            var result = ApplicationInformationProvider.GetApplicationInformation();
+
+            // Assert
+            Assert.IsNotNullOrEmpty(result.LogFolder);
+        }
+
+        [Test]
+        public void GetApplicationInformation_ExecutingUserIsSet()
+        {
+            // Act
+            var result = ApplicationInformationProvider.GetApplicationInformation();
+
+            // Assert
+            Assert.IsNotNull(result.ExecutingUser);
+        }
+
+        [Test]
+        public void GetApplicationInformation_MachineNameIsSet()
+        {
+            // Act
+            var result = ApplicationInformationProvider.GetApplicationInformation();
+
+            // Assert
+            Assert.IsNotNullOrEmpty(result.MachineName);
+        }
+    }
+}
