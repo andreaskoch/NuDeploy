477d0ff89e9cc4edda17d26440118b6063a83d18
Andreas Koch <andyk7@gmail.com>
2012-07-08 21:58:20 +0200

    Created a simple unit test for the DefaultFileEncodingProvider class

---
+++ source/Tests/UnitTests/Common/DefaultFileEncodingProviderTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+using NuDeploy.Core.Common;
+
+using NUnit.Framework;
+
+namespace NuDeploy.Tests.UnitTests.Common
+{
+    [TestFixture]
+    public class DefaultFileEncodingProviderTests
+    {
+        private IEncodingProvider encodingProvider;
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            this.encodingProvider = new DefaultFileEncodingProvider();
+        }
+
+        [Test]
+        public void GetEncoding_ResultIsNotNull()
+        {
+            // Act
+            var result = this.encodingProvider.GetEncoding();
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void GetEncoding_ResultIsUTF8()
+        {
+            // Act
+            var result = this.encodingProvider.GetEncoding();
+
+            // Assert
+            Assert.AreEqual(Encoding.UTF8, result);
+        }
+    }
+}
