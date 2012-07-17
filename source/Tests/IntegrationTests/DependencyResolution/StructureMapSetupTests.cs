9432e1807abc7a25d295f15f9278d1a0228e3f85
Andreas Koch <andyk7@gmail.com>
2012-07-17 17:26:53 +0200

    Added an integration test for the StructureMapSetup class

---
+++ source/Tests/IntegrationTests/DependencyResolution/StructureMapSetupTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Management.Automation.Host;
+using System.Runtime.InteropServices;
+
+using NuDeploy.Core.Common.FileEncoding;
+using NuDeploy.Core.Common.FilesystemAccess;
+using NuDeploy.Core.Common.Infrastructure;
+using NuDeploy.Core.Common.Logging;
+using NuDeploy.Core.Common.UserInterface;
+using NuDeploy.Core.Common.UserInterface.Console;
+using NuDeploy.Core.DependencyResolution;
+using NuDeploy.Core.Services.AssemblyResourceAccess;
+using NuDeploy.Core.Services.Cleanup;
+using NuDeploy.Core.Services.Commands;
+using NuDeploy.Core.Services.Console;
+using NuDeploy.Core.Services.Filesystem;
+using NuDeploy.Core.Services.Installation;
+using NuDeploy.Core.Services.Installation.Repositories;
+using NuDeploy.Core.Services.Packaging;
+using NuDeploy.Core.Services.Packaging.Build;
+using NuDeploy.Core.Services.Packaging.Configuration;
+using NuDeploy.Core.Services.Packaging.Nuget;
+using NuDeploy.Core.Services.Packaging.PrePackaging;
+using NuDeploy.Core.Services.Status;
+using NuDeploy.Core.Services.Transformation;
+using NuDeploy.Core.Services.Update;
+
+using NuGet;
+
+using NUnit.Framework;
+
+using StructureMap;
+
+namespace NuDeploy.Tests.IntegrationTests.DependencyResolution
+{
+    [TestFixture]
+    public class StructureMapSetupTests
+    {
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            StructureMapSetup.Setup();
+        }
+
+        [TestCase(typeof(IEncodingProvider))]
+        [TestCase(typeof(IActionLogger))]
+        [TestCase(typeof(ApplicationInformation))]
+        [TestCase(typeof(IFilesystemAccessor))]
+        [TestCase(typeof(IUserInterface))]
+        [TestCase(typeof(IConsoleTextManipulation))]
+        [TestCase(typeof(PSHost))]
+        [TestCase(typeof(PSHostUserInterface))]
+        [TestCase(typeof(IPackageRepositoryFactory))]
+        [TestCase(typeof(_Assembly))]
+        [TestCase(typeof(IAssemblyResourceFilePathProvider))]
+        [TestCase(typeof(IAssemblyFileResourceProvider))]
+        [TestCase(typeof(IAssemblyResourceDownloader))]
+        [TestCase(typeof(ICleanupService))]
+        [TestCase(typeof(IRelativeFilePathInfoFactory))]
+        [TestCase(typeof(ISourceRepositoryProvider))]
+        [TestCase(typeof(Func<Uri, IHttpClient>))]
+        [TestCase(typeof(IPackageRepositoryBrowser))]
+        [TestCase(typeof(IPackageConfigurationAccessor))]
+        [TestCase(typeof(IPackageInstaller))]
+        [TestCase(typeof(ISolutionPackagingService))]
+        [TestCase(typeof(IBuildFolderPathProvider))]
+        [TestCase(typeof(ISolutionBuilder))]
+        [TestCase(typeof(IBuildResultFilePathProvider))]
+        [TestCase(typeof(ICommandArgumentNameMatcher))]
+        [TestCase(typeof(ICommandArgumentParser))]
+        [TestCase(typeof(ICommandLineArgumentInterpreter))]
+        [TestCase(typeof(ICommandNameMatcher))]
+        [TestCase(typeof(IPackagingFolderPathProvider))]
+        [TestCase(typeof(IPackagingService))]
+        [TestCase(typeof(IPrePackagingFolderPathProvider))]
+        [TestCase(typeof(IPrepackagingService))]
+        [TestCase(typeof(IInstallationStatusProvider))]
+        [TestCase(typeof(IConfigurationFileTransformer))]
+        [TestCase(typeof(ISelfUpdateService))]
+        [TestCase(typeof(ICommandProvider))]
+        public void CanInstantiate_Interface(Type interfaceType)
+        {
+            // Act
+            var result = ObjectFactory.TryGetInstance(interfaceType);
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
