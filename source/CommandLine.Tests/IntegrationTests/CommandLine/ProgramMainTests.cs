29332de9fc0f64fd25104642d034960fb70c2c32
Andreas Koch <andyk7@gmail.com>
2012-07-28 22:54:51 +0200

    Created a integration test for the main method of the program class

---
+++ source/CommandLine.Tests/IntegrationTests/CommandLine/ProgramMainTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+
+using NUnit.Framework;
+
+namespace CommandLine.Tests.IntegrationTests.CommandLine
+{
+    [TestFixture]
+    public class ProgramMainTests
+    {
+        private readonly Mutex sequentialTestExecutionMonitor;
+
+        public ProgramMainTests()
+        {
+            this.sequentialTestExecutionMonitor = new Mutex(false);
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            this.sequentialTestExecutionMonitor.WaitOne();
+
+            CommandLineIntegrationTestUtilities.Cleanup();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            this.sequentialTestExecutionMonitor.ReleaseMutex();
+        }         
+    }
+}
