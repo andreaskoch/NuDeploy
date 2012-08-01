e28c06b8e451e713fca0034d761ed898e76c71dd
Andreas Koch <andyk7@gmail.com>
2012-08-01 21:52:29 +0200

    Created unit tests for the PackageServerFactory

---
+++ source/Core.Tests/UnitTests/Publishing/PackageServerFactoryTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+using NuDeploy.Core.Services.Publishing;
+
+using NUnit.Framework;
+
+namespace NuDeploy.Tests.UnitTests.Publishing
+{
+    [TestFixture]
+    public class PackageServerFactoryTests
+    {
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetPackageServer_ServerLocationParameterIsInvalid_ArgumentExceptionIsThrown(string serverLocation)
+        {
+            // Arrange
+            var packageServerFactory = new PackageServerFactory();
+
+            // Act
+            packageServerFactory.GetPackageServer(serverLocation);
+        }
+
+        [Test]
+        public void GetPackageServer_ServerLocationParameterIsValid_ResultIsNotNull()
+        {
+            // Arrange
+            var serverLocation = "http://nuget.org/api/v2";
+            var packageServerFactory = new PackageServerFactory();
+
+            // Act
+            var result = packageServerFactory.GetPackageServer(serverLocation);
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestCase("http://nuget.org/api/v2")]
+        [TestCase(@"C:\nuget-repository")]
+        [TestCase(@"\\unc-path\repository")]
+        public void GetPackageServer_ServerLocationParameterIsValid_PackageServerSourceParameterEqualsSuppliedServerLocation(string serverLocation)
+        {
+            // Arrange
+            var packageServerFactory = new PackageServerFactory();
+
+            // Act
+            var result = packageServerFactory.GetPackageServer(serverLocation);
+
+            // Assert
+            Assert.AreEqual(serverLocation, result.Source);
+        }
+    }
+}
