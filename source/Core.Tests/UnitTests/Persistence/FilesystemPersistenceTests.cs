31d5131200f4012092e9dbb869c0f6ee2dab019f
Andreas Koch <andyk7@gmail.com>
2012-08-02 21:58:22 +0200

    Started implementing unit tests for the FilesystemPersistence class

---
+++ source/Core.Tests/UnitTests/Persistence/FilesystemPersistenceTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+using Moq;
+
+using NuDeploy.Core.Common.FilesystemAccess;
+using NuDeploy.Core.Common.Persistence;
+using NuDeploy.Core.Common.Serialization;
+using NuDeploy.Core.Services.Publishing;
+
+using NUnit.Framework;
+
+namespace NuDeploy.Tests.UnitTests.Persistence
+{
+    [TestFixture]
+    public class FilesystemPersistenceTests
+    {
+        #region constructor
+
+        [Test]
+        public void Constructor_PublishConfiguration_AllParametersAreSet_ObjectIsInstantiated()
+        {
+            // Arrange
+            var filesystemAccessor = new Mock<IFilesystemAccessor>();
+            var objectSerializer = new Mock<IObjectSerializer<PublishConfiguration>>();
+
+            // Act
+            var filesystemPersistence = new FilesystemPersistence<PublishConfiguration>(filesystemAccessor.Object, objectSerializer.Object);
+
+            // Assert
+            Assert.IsNotNull(filesystemPersistence);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_PublishConfiguration_FilesystemAccessorParametersIsNotSet_ArgumentNullExceptionIsThrown()
+        {
+            // Arrange
+            var objectSerializer = new Mock<IObjectSerializer<PublishConfiguration>>();
+
+            // Act
+            new FilesystemPersistence<PublishConfiguration>(null, objectSerializer.Object);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_PublishConfiguration_ObjectSerializerParametersIsNotSet_ArgumentNullExceptionIsThrown()
+        {
+            // Arrange
+            var filesystemAccessor = new Mock<IFilesystemAccessor>();
+
+            // Act
+            new FilesystemPersistence<PublishConfiguration>(filesystemAccessor.Object, null);
+        }
+
+        #endregion
+    }
+}
